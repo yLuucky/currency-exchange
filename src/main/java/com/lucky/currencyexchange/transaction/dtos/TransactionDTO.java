b5a3b6d5edc447fd1322a72a9347af67037149bd
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2023-10-03 20:59:52 -0300

    feat: created transactionDTO

---
+++ src/main/java/com/lucky/currencyexchange/transaction/dtos/TransactionDTO.java
@@ -0,0 +1,30 @@
+package com.lucky.currencyexchange.transaction.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.lucky.currencyexchange.transaction.enums.Currency;
+import com.lucky.currencyexchange.user.infra.db.entities.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "create")
+public class TransactionDTO {
+
+    private UUID transactionId;
+    private User user;
+    private Currency initialCurrency;
+    private Currency finalCurrency;
+    private double originalValue;
+    private double conversionRate;
+
+    public double getConvertedValue() {
+        return originalValue * conversionRate;
+    }
+
+}
