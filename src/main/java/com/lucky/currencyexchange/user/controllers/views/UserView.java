b5cdda4d457bbaab3f75e784285c1db4f53b607a
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2023-10-03 23:45:32 -0300

    feat: created UserView

--- src/main/java/com/lucky/currencyexchange/user/controllers/views/UserView.java
+++ src/main/java/com/lucky/currencyexchange/user/controllers/views/UserView.java
@@ -1,4 +1,35 @@
 package com.lucky.currencyexchange.user.controllers.views;
 
+import com.lucky.currencyexchange.user.dtos.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@AllArgsConstructor(staticName = "create")
 public class UserView {
+
+    private UUID userId;
+    private String nickname;
+    private String email;
+    private String password;
+
+    public static UserView from(UserDTO userDTO) {
+        return UserView.create(
+                userDTO.getId(),
+                userDTO.getNickname(),
+                userDTO.getEmail(),
+                userDTO.getUser_pass()
+        );
+    }
+
+    public static List<UserView> from(List<UserDTO> userDTOS) {
+        return userDTOS.stream().map(UserView::from).collect(Collectors.toList());
+    }
+
 }
