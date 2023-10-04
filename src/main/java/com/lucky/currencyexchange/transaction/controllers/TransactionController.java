8adfbef80bb8d2e3d3235ed174fa00b43c3b8f1a
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2023-10-03 23:49:40 -0300

    ref: refactored to use new service

--- src/main/java/com/lucky/currencyexchange/transaction/controllers/TransactionController.java
+++ src/main/java/com/lucky/currencyexchange/transaction/controllers/TransactionController.java
@@ -4,53 +4,55 @@ import com.lucky.currencyexchange.transaction.controllers.views.TransactionView;
 import com.lucky.currencyexchange.transaction.dtos.TransactionDTO;
 import com.lucky.currencyexchange.transaction.services.ICreateTransactionService;
 import com.lucky.currencyexchange.transaction.services.IFindTransactionsByUserIdService;
-import com.lucky.currencyexchange.user.infra.db.repositories.jpa.UserRepository;
+import com.lucky.currencyexchange.user.services.IFindUserByIdService;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("transaction")
 public class TransactionController {
 
-    private final UserRepository userRepository;
     private final ICreateTransactionService createTransactionService;
     private final IFindTransactionsByUserIdService findTransactionsByUserIdService;
+    private final IFindUserByIdService findUserByIdService;
 
     @Autowired
     public TransactionController(final ICreateTransactionService createTransactionService,
                                  final IFindTransactionsByUserIdService findTransactionsByUserIdService,
-                                 final UserRepository userRepository) {
+                                 final IFindUserByIdService findUserByIdService) {
         this.findTransactionsByUserIdService = findTransactionsByUserIdService;
         this.createTransactionService = createTransactionService;
-        this.userRepository = userRepository;
+        this.findUserByIdService = findUserByIdService;
     }
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "created a transaction"),
-            @ApiResponse(responseCode = "400", description = "could not create")
+            @ApiResponse(responseCode = "400", description = "could not create a transaction")
     })
     @PostMapping
     public ResponseEntity<TransactionView> create(final TransactionDTO transactionDTO, final String userId) {
-        userRepository.findById(UUID.fromString(userId));
+        findUserByIdService.execute(UUID.fromString(userId));
         final TransactionDTO dto = createTransactionService.execute(transactionDTO);
         return ResponseEntity.status(201).body(TransactionView.from(dto));
     }
 
-    @ApiResponse(responseCode = "201", description = "created transaction")
-    @ApiResponse(responseCode = "400", description = "could not create")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "get transactions"),
+            @ApiResponse(responseCode = "400", description = "could not get transactions")
+    })
     @GetMapping("{id}")
     public ResponseEntity<List<TransactionView>> show(final String userId) {
-        final List<TransactionDTO> dtos = findTransactionsByUserIdService.execute(UUID.fromString(userId));
-        return ResponseEntity.ok(TransactionView.from(dtos));
+        final List<TransactionDTO> transactionDTOS = findTransactionsByUserIdService.execute(UUID.fromString(userId));
+        return ResponseEntity.ok(TransactionView.from(transactionDTOS));
     }
 
 }
