6bac8a8036a96820845433ee9f6d844c44a7333d
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2023-10-03 21:09:06 -0300

    feat: created TransactionMapper

---
+++ src/main/java/com/lucky/currencyexchange/transaction/mappers/TransactionMapper.java
@@ -0,0 +1,24 @@
+package com.lucky.currencyexchange.transaction.mappers;
+
+import com.lucky.currencyexchange.transaction.dtos.TransactionDTO;
+import com.lucky.currencyexchange.transaction.infra.db.entities.Transaction;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TransactionMapper {
+
+    public static Transaction mapper(final TransactionDTO transactionDTO) {
+        final Transaction transaction = new Transaction();
+        transaction.setUserId(transactionDTO.getUser().getUserId());
+
+        return transaction;
+    }
+
+    public static TransactionDTO mapper(final Transaction transaction) {
+        return TransactionDTO.create(transaction.getId(), null, transaction.getInitialCurrency(),
+                transaction.getFinalCurrency(), transaction.getOriginalValue(), transaction.getConversionRate());
+    }
+
+
+}
