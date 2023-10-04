f684341a364839b974fd7146d53cc7368032915e
Lucas Silveira Cardoso <57505844+yLuucky@users.noreply.github.com>
2023-10-03 23:46:47 -0300

    feat: created UserController

--- src/main/java/com/lucky/currencyexchange/user/controllers/UserController.java
+++ src/main/java/com/lucky/currencyexchange/user/controllers/UserController.java
@@ -1,4 +1,36 @@
 package com.lucky.currencyexchange.user.controllers;
 
+import com.lucky.currencyexchange.user.controllers.views.UserView;
+import com.lucky.currencyexchange.user.dtos.UserDTO;
+import com.lucky.currencyexchange.user.infra.db.entities.User;
+import com.lucky.currencyexchange.user.mappers.UserViewMapper;
+import com.lucky.currencyexchange.user.services.ICreateUserService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+
+    private final ICreateUserService createUserService;
+
+    @Autowired
+    public UserController(final ICreateUserService createUserService) {
+        this.createUserService = createUserService;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "created user"),
+            @ApiResponse(responseCode = "400", description = "could not create a user")
+    })
+    @PostMapping
+    public ResponseEntity<UserView> create(final UserDTO userDTO) {
+        final User user = createUserService.execute(userDTO);
+        return ResponseEntity.status(201).body(UserViewMapper.mapper(user));
+    }
 }
